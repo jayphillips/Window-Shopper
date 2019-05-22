ee7b84f2b8e391126f3fc1eeb7d9a1581584e524
Jay Phillips <jayphillips@icloud.com>
2019-05-21 23:50:21 -0400

    finished lesson-4

--- Window Shopper/Controller/MainVC.swift
+++ Window Shopper/Controller/MainVC.swift
@@ -14,6 +14,10 @@ class MainVC: UIViewController {
     
     @IBOutlet weak var priceTxt: CurrencyTxtField!
     
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    @IBOutlet weak var hoursLbl: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +30,28 @@ class MainVC: UIViewController {
         
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
     }
 
     @objc func calculate() {
-        print("We got here")
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
 
+    @IBAction func clearCalculatorBtnPressed(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
 }
 
