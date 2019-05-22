a2840881a214db5c16a2e7c3b816f2b1df19fe86
Jay Phillips <jayphillips@icloud.com>
2019-05-21 22:58:41 -0400

    finished lesson 1

---
+++ Window Shopper/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by Jay Phillips on 5/21/19.
+//  Copyright © 2019 Jay Phillips. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
